5718a0c740e5aa861864cb6f7bb56fb41e2bcf4e
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-29 20:33:32 -0500

    Add ability to choose currency.

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const button = document.querySelector('button');
+const form = document.querySelector('form');
 
-button.addEventListener('click', () => {
-  
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  new FormData(form); // fires teh formdata event
+});
+
+form.addEventListener('formdata', e => {
+
+  const currency = e.formData.get('currency');
+
+  const currencies = {
+    'USD': '$',
+    'EUR': '€',
+    'GBP': '£',
+  }
+
   const span = document.querySelector('span');
   const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener('load', function() {
     if (this.status !== 200) return console.error('Request failed!');
-    const rate = JSON.parse(this.response).bpi.USD.rate;
-    span.textContent = `$${rate}`;
+    const rate = JSON.parse(this.response).bpi[currency].rate;
+    span.textContent = `${currencies[currency]}${rate}`;
   });
 
   xhr.addEventListener('error', () => console.error('Request failed!'));
 
   xhr.open('GET', url);
   xhr.send();
-
 });
